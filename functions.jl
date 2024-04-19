c4fda4d1ba9ca25dd60519a6c867c4a02a320d22
Matt Heiden <MHeiden2@munichre.com>
2024-04-19 18:42:19 -0400

    simplified code

--- functions.jl
+++ functions.jl
@@ -11,3 +11,23 @@ function pick_a_card!(deck::Deck)
 end
 
 clear_screen() = print("\033c")
+
+function parse_card_text(text::AbstractString, deck_file::AbstractString)
+    question_regex = match(r"^[^\*]*(?=\r\n)", text)
+    if !isnothing(question_regex)
+        question = question_regex.match
+    else
+        println("Check ./deck/$deck_file for improperly formatted flashcard(s)")
+        readline()
+    end
+    answer = join((x -> x.match).(eachmatch(r"\*.*", text)), "\n")
+    return Card(question, answer)
+end
+function parse_card_text(texts::Vector{SubString{String}}, deck_file::AbstractString)
+    return [parse_card_text(text, deck_file) for text in texts]
+end
+
+function read_cards_from_deck_file(deck_file)
+    card_texts = split(read("./decks/$deck_file", String), "\r\n\r\n")
+    cards = parse_card_text(card_texts, deck_file)
+end
--- main.jl
+++ main.jl
@@ -1,23 +1,12 @@
 # Creating a flashcard program using the command line
-# For formulas I'll provide all variable names in the question
+using REPL.TerminalMenus
 include("structs.jl")
 include("functions.jl")
 
-deck = Deck([])
-for section in readdir("./decks")
-    cards = split(read("./decks/$section", String), "\r\n\r\n")
-    for card in cards
-        question_regex = match(r"^[^\*]*(?=\r\n)", card)
-        if !isnothing(question_regex)
-            question = question_regex.match
-        else
-            println("Check $section for improperly formatted flashcard(s)")
-            readline()
-        end
-        answer = join((x -> x.match).(eachmatch(r"\*.*", card)), "\n")
-        push!(deck.cards, Card(question, answer))
-    end
-end
+deck_files = readdir("./decks")
+#selected_decks = request("Please select from the following decks:", MultiSelectMenu(deck_files))
+selected_decks = deck_files
+deck = union(read_cards_from_deck_file.(selected_decks)...) |> Deck
 
 while length(deck.cards) > 0
     card = pick_a_card!(deck)
@@ -29,27 +18,27 @@ while length(deck.cards) > 0
     println("Did you get it right? (y/n)")
 
     response = readline()
-    is_valid_input = false
-    while !is_valid_input
+    while true
         if response == "n"
-            is_valid_input = true
-            println("Putting card back into the deck")
+            println("\nPutting card back into the deck")
             card.views += 1
             push!(deck.cards, card)
             readline()
+            break
         elseif response == "y"
             is_valid_input = true
             if length(deck.cards) > 0
-                printstyled("Great! :)\n Press any key for next flashcard\n",
-                    color=:light_green)
+                printstyled("\nGreat! :)\nPress enter for next flashcard\n",
+                color=:light_green)
                 readline()
             elseif length(deck.cards) == 0
                 printstyled("Congrats, you've finished the deck! :)
-                    \nPress enter to close", color=:light_green)
+                \nPress enter to close", color=:light_green)
                 readline()
             end
+            break
         else
-            println("$response is invalid input (must be y or n)")
+            println("\"$response\" is invalid input (must be y or n)")
             println("Did you get it right? (y/n)")
             response = readline()
         end
